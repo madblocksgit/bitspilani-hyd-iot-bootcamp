41f9cf0c06b0b22e11f108be686a734740d91825
MS <72651078+madblocksgit@users.noreply.github.com>
2021-07-27 14:48:37 +0530

    Create button_pullup_led.ino

---
+++ day1/button_pullup_led.ino
@@ -0,0 +1,16 @@
+
+int button=6;
+int led=3;
+
+void setup() {
+  pinMode(button,INPUT_PULLUP); // read permission
+  pinMode(led,OUTPUT); // write permission
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(digitalRead(button)==0) // on (pressed)
+    digitalWrite(led,1);
+  else
+    digitalWrite(led,0); // off (not pressed)
+}
