baa26ef6be91482862f4c6b661efa27b88f7cdab
MS <72651078+madblocksgit@users.noreply.github.com>
2021-07-29 14:04:34 +0530

    Create arduino-relay.ino

---
+++ day3/arduino-relay.ino
@@ -0,0 +1,17 @@
+int relay=2;
+
+void setup() {
+ pinMode(relay,OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+ while(Serial.available()) {
+  String s=Serial.readString(); 
+  if(s=="on") {
+    digitalWrite(relay,0); // on
+  } else if(s=="off") {
+    digitalWrite(relay,1); // off
+  }
+ }
+}
